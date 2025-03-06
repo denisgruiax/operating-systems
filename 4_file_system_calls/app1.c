12e56eee10f51a820d67454f2d5c7d2d0bec5a19
Denis Gruia <denis.gruiax@icloud.com>
2025-03-06 21:24:10 +0200

    Opening a file printing the content until end of the file in a fixed buffer and display its content every iteration

--- 4_file_system_calls/app1.c
+++ 4_file_system_calls/app1.c
@@ -1,15 +1,32 @@
+/*
+    Opening a file printing the content until end of the file in a fixed buffer and display its content every iteration.
+*/
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
-int main(int argc, char *argv[]){
+#define BUFFER_SIZE 10
+
+int main(int argc, char *argv[])
+{
+    int bytes_read = 0;
     int file_descriptor = open("file.txt", O_RDONLY);
-    char buffer[100] = "";
-    read(file_descriptor, &buffer, 100);
+    char buffer[BUFFER_SIZE] = {};
 
-    printf("descriptor value: %i\n", file_descriptor);
-    printf("content:\n\"%s\"\n", buffer);
+    printf("content:\n");
+
+    while ((bytes_read = read(file_descriptor, buffer, BUFFER_SIZE)) > 0)
+    {
+        write(STDOUT_FILENO, buffer, bytes_read);
+    }
+
+    if (bytes_read == -1)
+    {
+        perror("Reading the file have failed");
+    }
 
     close(file_descriptor);
 }
