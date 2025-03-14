00835f1cec2d35f5430ec3afd5321de80b5cc337
Denis Gruia <denis.gruiax@icloud.com>
2025-03-14 20:21:50 +0200

    Forked first child and parsed a message between them with the first pipe

--- 5_processes_in_unix_systems/app2/app2.c
+++ 5_processes_in_unix_systems/app2/app2.c
@@ -12,14 +12,17 @@ Write a program that creates three processes, as follows:
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <string.h>
 
 #define NUMBER_OF_PIPES 3
-#define BUFFER_SIZE 4098
+#define BUFFER_SIZE 1024
+#define MAX_TEXT_SIZE 4098
 
 void init_pipes(int pipes[][2], unsigned size);
 void check_pipes(int pipes[][2], unsigned size);
 void close_pipes(int pipes[][2], unsigned size);
 void check_process_creation(pid_t id);
+void check_fd(int fd);
 
 int main(int argc, char *argv[])
 {
@@ -28,10 +31,50 @@ int main(int argc, char *argv[])
 
     init_pipes(pipes, NUMBER_OF_PIPES);
     check_pipes(pipes, NUMBER_OF_PIPES);
-    
-    close_pipes(pipes, NUMBER_OF_PIPES);
 
-    return EXIT_SUCCESS;
+    child1 = fork();
+    check_process_creation(child1);
+
+    if (!child1)
+    {
+        close(pipes[0][1]);
+
+        char data_from_parent[MAX_TEXT_SIZE] = {};
+        
+        while (read(pipes[0][0], data_from_parent, BUFFER_SIZE) > 0)
+            ;
+        printf("Data from parent: %s\n", data_from_parent);
+
+        close_pipes(pipes, NUMBER_OF_PIPES);
+    }
+    else
+    {
+        child2 = fork();
+        check_process_creation(child2);
+
+        if (!child2)
+        {
+            /* code */
+        }
+        else
+        {
+            char data[MAX_TEXT_SIZE] = {};
+            int data_fd = open("data.txt", O_RDONLY);
+
+            check_fd(data_fd);
+
+            while (read(data_fd, data, BUFFER_SIZE) > 0)
+                ;
+
+            int write_buffer = strlen(data);
+            while (write(pipes[0][1], data, write_buffer) < write_buffer)
+                ;
+
+            close_pipes(pipes, NUMBER_OF_PIPES);
+
+            return EXIT_SUCCESS;
+        }
+    }
 }
 
 void init_pipes(int pipes[][2], unsigned size)
@@ -57,10 +100,20 @@ void close_pipes(int pipes[][2], unsigned size)
         close(pipes[i][0]), close(pipes[i][0]);
 }
 
-void check_process_creation(pid_t id){
+void check_process_creation(pid_t id)
+{
     if (id == -1)
     {
         perror("Failed to create new process with fork.");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void check_fd(int fd)
+{
+    if (fd ==-1)
+    {
+        perror("Failed opening the file.\n");
         exit(EXIT_FAILURE);
     }
 }
