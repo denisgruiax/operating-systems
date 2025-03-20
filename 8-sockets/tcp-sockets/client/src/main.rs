87d2a8d178ec6efbc6df03ea69eceff25a76b128
Denis Gruia <denis.gruiax@icloud.com>
2025-03-20 14:14:35 +0200

    Fixed client core logic to read all the bytes from server

--- 8-sockets/tcp-sockets/client/src/main.rs
+++ 8-sockets/tcp-sockets/client/src/main.rs
@@ -1,12 +1,23 @@
+use std::io::{Read, Write};
 use std::net::TcpStream;
-use std::io::{Write, Read};
 
 fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
-    stream.write(b"abcdefghi").unwrap();
+    stream.write(b"Hello server!").unwrap();
 
-    let mut buffer = [0,128];
-    stream.read(&mut buffer).unwrap();
-    
-    println!("Server response: {}", String::from_utf8_lossy(&buffer));
+    let mut response = String::new();
+    let mut buffer = [0; 128];
+
+    while let Ok(bytes_read) = stream.read(&mut buffer) {
+        if bytes_read == 0 {
+            break;
+        }
+        response.push_str(&String::from_utf8_lossy(&buffer[..bytes_read]));
+    }
+
+    println!("response: {}\n", response);
 }
+
+/*
+    Client might need to loop because TCP may deliver the response in multiple parts.
+ */
