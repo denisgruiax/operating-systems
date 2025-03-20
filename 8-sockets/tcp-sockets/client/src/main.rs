10da4e9c41ab41a014c24bf981b9014759a5ba56
Denis Gruia <denis.gruiax@icloud.com>
2025-03-20 13:16:33 +0200

    Implemented the core logic to connect, write to a server and read the response from it

--- 8-sockets/tcp-sockets/client/src/main.rs
+++ 8-sockets/tcp-sockets/client/src/main.rs
@@ -2,5 +2,11 @@ use std::net::TcpStream;
 use std::io::{Write, Read};
 
 fn main() {
-    println!("Hello, world!");
+    let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
+    stream.write(b"abcdefghi").unwrap();
+
+    let mut buffer = [0,128];
+    stream.read(&mut buffer).unwrap();
+    
+    println!("Server response: {}", String::from_utf8_lossy(&buffer));
 }
