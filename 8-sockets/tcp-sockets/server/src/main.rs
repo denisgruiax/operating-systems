338d5a86e0c8fa525d5fa322ee71c4397edf924a
Denis Gruia <denis.gruiax@icloud.com>
2025-03-20 13:11:27 +0200

    Implemnted the core logic to start a basic server using TcpListener library

--- 8-sockets/tcp-sockets/server/src/main.rs
+++ 8-sockets/tcp-sockets/server/src/main.rs
@@ -1,6 +1,22 @@
-use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Server running on 127.0.0.1:8080 ⚡️");
+
+    listener.incoming().for_each(|s| handle_client(s.unwrap()));
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer: [u8; 128] = [0; 128];
+
+    stream.read(&mut buffer).iter().for_each(|bytes_size| {
+        println!(
+            "Received from {} bytes from the client: {:?}",
+            bytes_size,
+            String::from_utf8_lossy(&buffer)
+        );
+        stream.write(b"Message received! :)").unwrap();
+    });
 }
