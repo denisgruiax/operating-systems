a3205b9abf8aaa895c26742bfd5f0b6bfc3f9062
Denis Gruia <denis.gruiax@icloud.com>
2025-03-21 23:40:21 +0200

    Set a better message for server

--- 8-sockets/udp-sockets/client/src/main.rs
+++ 8-sockets/udp-sockets/client/src/main.rs
@@ -5,10 +5,10 @@ fn main() -> std::io::Result<()> {
     let socket = UdpSocket::bind("127.0.0.1:0")?;
     socket.connect("127.0.0.1:8080")?;
 
-    socket.send("Hello UDP server".as_bytes())?;
+    socket.send("Generate a random number".as_bytes())?;
 
     let amt = socket.recv(&mut buffer)?;
-    
+
     println!("Received: {}", String::from_utf8_lossy(&buffer[..amt]));
 
     Ok(())
