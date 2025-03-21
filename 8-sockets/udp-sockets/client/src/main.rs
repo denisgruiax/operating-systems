b0957930bfb163ac94d439c34bc69d75b4cec7f0
Denis Gruia <denis.gruiax@icloud.com>
2025-03-21 23:33:15 +0200

    Implemented the UDP client

--- 8-sockets/udp-sockets/client/src/main.rs
+++ 8-sockets/udp-sockets/client/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::UdpSocket;
+
+fn main() -> std::io::Result<()> {
+    let mut buffer = [0; 128];
+    let socket = UdpSocket::bind("127.0.0.1:0")?;
+    socket.connect("127.0.0.1:8080")?;
+
+    socket.send("Hello UDP server".as_bytes())?;
+
+    let amt = socket.recv(&mut buffer)?;
+    
+    println!("Received: {}", String::from_utf8_lossy(&buffer[..amt]));
+
+    Ok(())
 }
