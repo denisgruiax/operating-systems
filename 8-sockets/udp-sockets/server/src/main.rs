8c64dcb0c50fa14b0efc5167097cae2fa9795378
Denis Gruia <denis.gruiax@icloud.com>
2025-03-21 23:22:28 +0200

    Implemented the core logic for UDP server

--- 8-sockets/udp-sockets/server/src/main.rs
+++ 8-sockets/udp-sockets/server/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::UdpSocket;
+
+fn main() -> std::io::Result<()> {
+    let mut buffer = [0; 128];
+    let socket = UdpSocket::bind("127.0.0.1:8080")?;
+
+    println!("UDP server is listening on 127.0.0.1:8080");
+
+    std::iter::repeat_with(|| {
+        socket
+            .recv_from(&mut buffer)
+            .map(|(amt, src)| (amt, src, buffer))
+    })
+    .map_while(Result::ok)
+    .try_for_each(|(amt, src, buffer)| {
+        println!(
+            "Received {} from {}",
+            String::from_utf8_lossy(&buffer[..amt]),
+            src
+        );
+
+        Ok::<(), std::io::Error>(())
+    })?;
+
+    Ok(())
 }
