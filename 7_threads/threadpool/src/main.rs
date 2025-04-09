199bab43929175517fcf6bd3725b8064f87b59bf
Denis Gruia <denis.gruiax@icloud.com>
2025-04-09 11:09:57 +0300

    Added a TCP Listener and a function to handle the connections.

--- 7_threads/threadpool/src/main.rs
+++ 7_threads/threadpool/src/main.rs
@@ -1,5 +1,5 @@
 use core::result::Result;
-use std::net::TcpListener;
+use std::{io::{BufRead, BufReader}, net::{TcpListener, TcpStream}};
 
 fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
@@ -7,7 +7,18 @@ fn main() -> Result<(), std::io::Error> {
     listener
         .incoming()
         .into_iter()
-        .for_each(|stream| println!("{:?}", stream));
+        .for_each(|stream| handle_connection(stream.unwrap()));
 
     Ok(())
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let buffer_reader = BufReader::new(&stream);
+    let http_request:Vec<_> = buffer_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line|!line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
 }
