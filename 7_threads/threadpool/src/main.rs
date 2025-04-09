f992981e7ec75e7ad0931f13a698a72a1264ac5a
Denis Gruia <denis.gruiax@icloud.com>
2025-04-09 11:28:45 +0300

    Printing the request and respond with an OK status

--- 7_threads/threadpool/src/main.rs
+++ 7_threads/threadpool/src/main.rs
@@ -1,5 +1,5 @@
 use core::result::Result;
-use std::{io::{BufRead, BufReader}, net::{TcpListener, TcpStream}};
+use std::{io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
 
 fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
@@ -7,12 +7,12 @@ fn main() -> Result<(), std::io::Error> {
     listener
         .incoming()
         .into_iter()
-        .for_each(|stream| handle_connection(stream.unwrap()));
+        .for_each(|stream| handle_connection(stream.unwrap()).unwrap());
 
     Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream){
+fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error>{
     let buffer_reader = BufReader::new(&stream);
     let http_request:Vec<_> = buffer_reader
         .lines()
@@ -20,5 +20,9 @@ fn handle_connection(mut stream: TcpStream){
         .take_while(|line|!line.is_empty())
         .collect();
 
-    println!("Request: {http_request:#?}");
+    println!("HTTP Request: {http_request:#?}");
+
+    stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())?;
+
+    Ok(())
 }
