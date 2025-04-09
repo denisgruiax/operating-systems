afc5794ac5a939c2c41e13ea76a8dfcd8be83cdb
Denis Gruia <denis.gruiax@icloud.com>
2025-04-09 11:43:21 +0300

    Returning the html file and an ok to the client

--- 7_threads/threadpool/src/main.rs
+++ 7_threads/threadpool/src/main.rs
@@ -1,5 +1,9 @@
 use core::result::Result;
-use std::{io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
@@ -12,17 +16,21 @@ fn main() -> Result<(), std::io::Error> {
     Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error>{
+fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error> {
     let buffer_reader = BufReader::new(&stream);
-    let http_request:Vec<_> = buffer_reader
+    let http_request: Vec<_> = buffer_reader
         .lines()
         .map(|result| result.unwrap())
-        .take_while(|line|!line.is_empty())
+        .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("HTTP Request: {http_request:#?}");
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html")?;
+    let length = contents.len();
 
-    stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())?;
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes())?;
 
     Ok(())
 }
