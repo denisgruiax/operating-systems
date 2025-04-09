d2fc55c7e742957b466667417cbcbacb08a635eb
Denis Gruia <denis.gruiax@icloud.com>
2025-04-09 13:22:32 +0300

    Simulating a longer task and give the response after 5 seconds

--- 7_threads/threadpool/src/main.rs
+++ 7_threads/threadpool/src/main.rs
@@ -3,6 +3,8 @@ use std::{
     fs,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    os::unix::thread,
+    time::Duration,
 };
 
 fn main() -> Result<(), std::io::Error> {
@@ -20,10 +22,13 @@ fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error> {
     let buffer_reader = BufReader::new(&stream);
     let request_line = buffer_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            std::thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "error.html"),
     };
 
     let contents = fs::read_to_string(filename)?;
