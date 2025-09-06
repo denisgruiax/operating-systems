7d34e67e0df896137e036b5ca06229a17940e079
Denis Gruia <denis.gruiax@icloud.com>
2025-09-06 10:04:13 +0300

    Simple mini redis hello world example

--- asynchronous/my-redis/src/main.rs
+++ asynchronous/my-redis/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use mini_redis::{Result, client};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    client.set("hello", "world".into()).await?;
+
+    let result = client.get("hello").await?;
+
+    println!("The value of the key is: {:?}", result);
+
+    Ok(())
 }
